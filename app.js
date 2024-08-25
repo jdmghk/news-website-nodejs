37f6b963b4bb6fd6bb4915c08a8e3cac53a9e581
JDK <jacinthdavid70@gmail.com>
2024-08-25 12:40:03 +0100

    added vercel.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 
 // Static Files
@@ -26,4 +26,4 @@ app.use('/article', newsRouter)
 
 
 //Listen on port 5000
-app.listen(port, ()=> console.log(`Listening on port ${port}`))
+app.listen(port, ()=> console.log(`Server is running on http://localhost:${port}`))
